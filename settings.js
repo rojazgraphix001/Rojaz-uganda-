6ca6edae3175b56e4f5fc261ef937d9011133b11
Rojaz uganda <rojagraphix0@gmail.com>
2025-05-08 11:47:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'ROJAZ UGANDA',
+  author: '‎ROJAZ',
+  botName: "ROJA BOTS",
+  botOwner: 'Rojaz Uganda', // Your name
+  ownerNumber: '256726991705', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "ROJA TECH UGANDA .",
   version: "2.0.0",
 };
 
